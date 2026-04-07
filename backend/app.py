6628a7f84961a8aa2605fbd434c8186478c6f6fb
Alina Hassan <161017071+AlinaHassan114@users.noreply.github.com>
2026-04-07 09:50:36 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -19,4 +19,4 @@ def get_student(reg_no):
         return jsonify({"error": "Student not found"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000 , debug=True)
